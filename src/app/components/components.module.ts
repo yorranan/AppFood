a326bdacad6f55cd22595f61efa82de633f9ad3e
yorranan <yorranan.costa@outlook.com>
2024-02-15 12:57:12 -0300

    aula 3

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { IonicModule } from '@ionic/angular';
+import { LoadingRestaurantComponent } from './loading-restaurant/loading-restaurant.component';
+import { EmptyScreenComponent } from './empty-screen/empty-screen.component';
+
+
+
+@NgModule({
+  declarations: [RestaurantComponent, LoadingRestaurantComponent, EmptyScreenComponent],
+  imports: [
+    CommonModule,
+    IonicModule
+  ], exports: [RestaurantComponent, LoadingRestaurantComponent, EmptyScreenComponent]
+})
+export class ComponentsModule { }
--- src/app/pages/tabs/search/search.module.ts
+++ src/app/pages/tabs/search/search.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { SearchPageRoutingModule } from './search-routing.module';
 
 import { SearchPage } from './search.page';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    ComponentsModule,
     SearchPageRoutingModule
   ],
   declarations: [SearchPage]
