b6cd71867ac506bca2a2b031703fb61dbd647a0b
Yorranan <yorranan.costa@outlook.com>
2024-02-23 18:32:32 -0300

    aula 23/02 ok

--- src/app/pages/tabs/cart/cart.page.ts
+++ src/app/pages/tabs/cart/cart.page.ts
@@ -8,18 +8,17 @@ import { Preferences } from '@capacitor/preferences';
   styleUrls: ['./cart.page.scss'],
 })
 export class CartPage implements OnInit {
-
   model: any = {};
   deliveryCharge = 20;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
-    this.getCartData(); 
+    this.getCartData();
   }
 
   getCart() {
-    return Preferences.get({key: 'cart'});
+    return Preferences.get({ key: 'cart' });
   }
   async getCartData() {
     let data: any = await this.getCart();
@@ -31,36 +30,37 @@ export class CartPage implements OnInit {
   }
 
   async calculate() {
-    let item = this.model.filter(x => x.quantity > 0);
+    let item = this.model.items.filter((x) => x.quantity > 0);
     this.model.items = item;
     this.model.totalPrice = 0;
     this.model.totalItem = 0;
     this.model.deliverycharge = 0;
-    this.model.grandTotal = 0; 
-    item.forEach(element => {
+    this.model.grandTotal = 0;
+    item.forEach((element) => {
       this.model.totalItem += element.quantity;
-      this.model.totalPrice += (parseFloat(element.price)) * (parseFloat(element.quantity))
+      this.model.totalPrice +=
+        parseFloat(element.price) * parseFloat(element.quantity);
     });
     this.model.deliverycharge = this.deliveryCharge;
     this.model.totalPrice = parseFloat(this.model.totalPrice).toFixed(2);
-    this.model.grandTotal (parseFloat(this.model.totalPrice) + parseFloat(this.model.deliverycharge)).toFixed(2); 
+    this.model.grandTotal = (
+      parseFloat(this.model.totalPrice) + parseFloat(this.model.deliverycharge)
+    ).toFixed(2);
     if (this.model.totalItem == 0) {
-      this.model.totalItem = 0
+      this.model.totalItem = 0;
       this.model.totalPrice = 0;
       this.model.grandTotal = 0;
       await this.clearCart();
-      this.model = 0; 
+      this.model = 0;
     }
   }
 
   clearCart() {
-    return Preferences.remove({key: 'cart'});
+    return Preferences.remove({ key: 'cart' });
   }
 
-  quantityPlus(index) {
-  }
-  quantityMinus(index) {
-  }
+  quantityPlus(index) {}
+  quantityMinus(index) {}
   addAdress() {}
   changeAddress() {}
 }
--- src/app/pages/tabs/items/items.page.ts
+++ src/app/pages/tabs/items/items.page.ts
@@ -217,9 +217,8 @@ export class ItemsPage implements OnInit {
   }
 
   async viewCart() {
-    if (this.cartData.items && this.cartData.items.length > 0)
-      await this.saveToCart();
-    this.router.navigate([this.router.url + 'cart']);
+    if (this.cartData.items && this.cartData.items.length > 0) await this.saveToCart();
+    this.router.navigate([this.router.url + '/cart']);
   }
 
   getCuisine(cuisine) {
